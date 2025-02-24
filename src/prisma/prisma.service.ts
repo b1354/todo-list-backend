fd81df0ffe122d370fa74709ab7fda5fd63aef29
b1354 <bayurizkyseptiansyah@gmail.com>
2025-02-24 15:39:27 +0700

    memperbaiki swagger

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,6 +4,10 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    await this.$connect();
+    try {
+      await this.$connect();
+    } catch (error) {
+      console.error('Error connecting to database:', error.message);
+    }
   }
 }
